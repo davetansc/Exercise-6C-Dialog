dd849e8ca94eea570ee1dfbde317c125b83aa772
davetansc <davetan@iu.edu>
2024-04-23 14:18:11 -0400

    delte

---
+++ models/Sprits/NashMeshAgent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+internal class NashMeshAgent
+{
+    internal void SetDestination(Vector3 point)
+    {
+        throw new NotImplementedException();
+    }
+}
