a91ecb2e8ec609c303c6a5cbc3d6277c7bba872b
davetansc <davetan@iu.edu>
2024-04-28 17:17:40 -0400

    add

---
+++ Assets/CS.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class CS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private NPCConversation myConversation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+
+            }
+        }
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
