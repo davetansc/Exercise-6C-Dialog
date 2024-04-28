76c7fa364f0ad9dbf4baac134e433d9740790445
davetansc <davetan@iu.edu>
2024-04-28 16:40:54 -0400

    add

---
+++ Assets/Scripts/playercoin.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class playercoin : MonoBehaviour
+{
+    public Text ui;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("coin"))
+        {
+            score++;
+            ui.text = "score :" + score;
+            Destroy(other.gameObject);
+        }
+    }
+
+}
